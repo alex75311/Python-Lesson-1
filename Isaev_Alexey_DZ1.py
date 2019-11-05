06a724b28b6c83126d34d3f71b97358e46761b00
Isaev Aleksey <isaev.alex75311@gmail.com>
2019-11-06 01:47:18 +0300

    step 3

--- Isaev_Alexey_DZ1.py
+++ Isaev_Alexey_DZ1.py
@@ -13,21 +13,29 @@
 # str1 = input('Введите первую строку ')
 # str2 = input('Введите вторую строку ')
 # print(f''' Первое число {int1} \n Второе число {int2} \n Первая строка {str1} \n Вторая строка {str2} ''')
+#
+# # step2
+# time_sec = int(input('Введите время в секундах '))
+# hour, minutes, sec = 0, 0, 0
+# if time_sec >= 60:
+#     minutes = time_sec // 60
+#     sec = time_sec % 60
+#     if minutes >= 60:
+#         hour = minutes // 60
+#         minutes = minutes % 60
+# if sec < 10:
+#     sec = '0' + str(sec)
+# if minutes < 10:
+#     minutes = '0' + str(minutes)
+# if hour < 10:
+#     hour = '0' + str(hour)
+# print(f'{hour}:{minutes}:{sec}')
+#
+# step3
+n = int(input('Вариант 1. Введите число '))
+result = n + (n*11) + (n*111)
+print(result)
 
-# step2
-time_sec = int(input('Введите время в секундах '))
-hour, minutes, sec = 0, 0, 0
-if time_sec >= 60:
-    minutes = time_sec // 60
-    sec = time_sec % 60
-    if minutes >= 60:
-        hour = minutes // 60
-        minutes = minutes % 60
-if sec < 10:
-    sec = '0' + str(sec)
-if minutes < 10:
-    minutes = '0' + str(minutes)
-if hour < 10:
-    hour = '0' + str(hour)
-print(f'{hour}:{minutes}:{sec}')
-
+n = int(input('Вариант 2. Введите число '))
+result = n + int(str(n)+str(n)) + int(str(n)+str(n)+str(n))
+print(result)
